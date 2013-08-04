fa929b3353eccd09eea9574b54875dafc15d2da7
Thomas ten Cate <ttencate@gmail.com>
2013-08-04 10:23:05 +0100

    Solution that does not inherit from String

--- ruby/rna-transcription/dna.rb
+++ ruby/rna-transcription/dna.rb
@@ -1,15 +1,37 @@
-class RibonucleicAcid < String
+class Acid
   def initialize(nucleotides)
-    super(nucleotides.to_s.gsub(/[^ACUG]/, ''))
+    @nucleotides = nucleotides.to_s.upcase.delete("^#{self.class.valid_nucleotides}")
+  end
+
+  def to_s
+    @nucleotides
+  end
+
+  def to_str
+    @nucleotides
+  end
+
+  def ==(other)
+    to_s == other.to_s
+  end
+
+  def self.valid_nucleotides
+    'ACG'
   end
 end
 
-class DeoxyribonucleicAcid < String
-  def initialize(nucleotides)
-    super(nucleotides.to_s.gsub(/[^ACTG]/, ''))
+class RibonucleicAcid < Acid
+  def self.valid_nucleotides
+    super + 'U'
+  end
+end
+
+class DeoxyribonucleicAcid < Acid
+  def to_rna
+    RibonucleicAcid.new(@nucleotides.tr('T', 'U'))
   end
 
-  def to_rna
-    RibonucleicAcid.new(gsub(/T/, 'U'))
+  def self.valid_nucleotides
+    super + 'T'
   end
 end
