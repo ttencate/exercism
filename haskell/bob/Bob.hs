6ba5b1ae9c22d6f8a0c0c65f194e079188bc3b35
Thomas ten Cate <ttencate@gmail.com>
2013-10-09 19:06:02 +0100

    Remove isIndifferent

--- haskell/bob/Bob.hs
+++ haskell/bob/Bob.hs
@@ -5,10 +5,9 @@ import Data.Char
 isSilence = all isSpace
 isYelling s = (any isUpper s) && (not $ any isLower s)
 isQuestion = (== '?') . last
-isIndifferent = const True
 
 responseFor s
-  | isSilence s     = "Fine. Be that way!"
-  | isYelling s     = "Woah, chill out!"
-  | isQuestion s    = "Sure."
-  | isIndifferent s = "Whatever."
+  | isSilence s  = "Fine. Be that way!"
+  | isYelling s  = "Woah, chill out!"
+  | isQuestion s = "Sure."
+  | otherwise    = "Whatever."
